39a1084a17137bf11052aff323dfa203b60446e5
Shital karek <118963232+shitalkarek@users.noreply.github.com>
2024-01-04 17:58:47 +0530

    Add files via upload

---
+++ my-portfolio/script.js
@@ -0,0 +1,71 @@
+function toggleMenu(){
+    const menu = document.querySelector(".menu-links");
+    const icon = document.querySelector(".hamburger-icon");
+    menu.classList.toggle("open");
+    icon.classList.toggle("open");
+}
+
+
+
+// dark and light mode
+
+const btn = document.getElementById("modeToggle");
+
+const btn2 = document.getElementById("modeToggle2");
+
+const themeIcons = document.querySelectorAll(".icon");
+const currentTheme = localStorage.getItem("theme");
+
+
+if (currentTheme === "dark")
+{
+    setDarkMode();
+}
+
+btn.addEventListener("click", function (){
+    setTheme();
+});
+
+btn2.addEventListener("click", function (){
+    setTheme();
+});
+
+function setTheme()
+{
+    let currentTheme = document.body.getAttribute("theme");
+
+    if(currentTheme === "dark")
+    {
+        //logic
+
+        setLightMode();
+    }
+    else {
+        setDarkMode();
+    }
+}
+
+function setDarkMode(){
+    //logic
+
+    document.body.setAttribute("theme" , "dark");
+    localStorage.setItem("theme", "dark");
+
+    themeIcons.forEach((icon) => {
+        icon.src = icon.getAttribute("src-dark"); 
+    });
+
+}
+
+function setLightMode(){
+ 
+    //logic
+
+
+    document.body.removeAttribute("theme");
+    localStorage.setItem("theme", "light");
+
+    themeIcons.forEach((icon) => {
+        icon.src = icon.getAttribute("src-light"); 
+    });
+}
